2d51a8d295937d8c379913704011dd5e6140dca9
fundon <cfddream@gmail.com>
2016-07-15 00:19:00 +0800

    Modal

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import BaseModal from './BaseModal'
+import CardModal from './CardModal'
+import ImageModal from './ImageModal'
+import Modal from './Modal'
+
+export {
+  BaseModal,
+  CardModal,
+  ImageModal,
+  Modal,
+}
